41f0637b775deca6ec1d467de75efa506400ae53
Bruno <brunorbds50@gmail.com>
2020-09-25 19:59:30 -0300

    Commit Final

--- src/br/com/digitalhouse/desafio/Professor1.kt
+++
@@ -1,8 +0,0 @@
-package br.com.digitalhouse.desafio
-
-interface Professor1 {
-    open val nome: String
-    open val sobrenome: String
-    open var TempoDeCasa: Int
-    open val codigoProfessor: Int
-}
